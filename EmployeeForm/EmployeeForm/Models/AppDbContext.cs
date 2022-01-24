f4f245059b7f6c4e0e0339cc689d091fbc9f54c1
Letjobana <suzanm@gerbertech.co.za>
2022-01-24 13:43:04 +0200

    Updated the appsetting and startup class and added a member class and dbcontext

---
+++ EmployeeForm/EmployeeForm/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeForm.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Member> members { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EmployeeForm/EmployeeForm/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeForm.Models
+{
+    public class Member
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string FirstName { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string LastName { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string MiddleName { get; set; }
+    }
+}
